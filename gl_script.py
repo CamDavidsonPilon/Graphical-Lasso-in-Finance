03ffbdb40602d3b4ff6761a44d43c821158a68ed
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2012-10-15 01:16:08 -0400

    cool changes.try affinity clustering next

--- gl_script.py
+++ gl_script.py
@@ -5,7 +5,7 @@ from sklearn.covariance import GraphLasso
 from pandas.io.data import DataReader
 import matplotlib
 matplotlib.use("Agg")
-
+from sklearn.preprocessing import scale
 
 import matplotlib.pyplot as plt
 from gl_tools import *
@@ -15,16 +15,25 @@ from graphical_lasso import graphical_lasso
 np.set_printoptions( precision = 5, suppress=True)
 
 
-symbols =['MS','BAC','K','PEP', 'KO', 'PG', 'MCD', 'WMT', 'JPM','C', 'WFC', 'GE', 'T', 'VZ', 'MSFT','GOOG', 'AAPL', 'RIMM']
-start_data = start=datetime.datetime(2009, 01, 03)
-data = dict( (sym, DataReader(sym, "yahoo", start = start_data) ) for sym in symbols )
+def get_data( symbols, start_date):
+	data = {}
+	new_symbols = []
+	for sym in symbols:
+		try:
+			data[sym] = DataReader( sym, "yahoo", start = start_date)
+			new_symbols.append( sym )
+		except:
+			pass
+	return new_symbols,data
+
+symbols =['MS','TOT','F', 'TM', 'MTU', 'TWX', 'CVX', 'MAR', '3M', 'HMC', 'PG', 'SNE', 'CAJ', 'VL0','BAC','K','PFE', 'XRX', 'AIG', 'PEP', 'KO', 'PG', 'MCD', 'WMT', 'JPM','C', 'WFC', 'GE', 'T', 'VZ', 'IBM', 'MSFT','GOOG', 'AAPL', 'RIMM']
+start_data = datetime.datetime(2010, 01, 03)
+symbols, data = get_data( symbols, start_data)
 close_data = np.concatenate( [ absolute_daily_returns( data[ts] )[:,None] for ts in symbols ], axis=1 )
-alpha = 0.00015
+alpha = 0.38
 
-gl = GraphLasso( alpha = alpha )
-
-gl.fit( close_data )
-
+gl = GraphLasso(alpha)
+nclose_data = scale(close_data)
 #save_network_graph( gl_pre, symbols, "Precision Matrix Network", "test.pdf" )
 #print gl_cov
 #print
--- gl_tools.py
+++ gl_tools.py
@@ -65,14 +65,14 @@ def save_network_graph_sequence( data, alpha_seq, labels, filename):
 		pos_labels = nx.circular_layout( D )
 		for k,item in pos_labels.iteritems():
 			pos_labels[k] = item + 0.1
-		nx.draw_circular( D, scale = 4, node_size = 150, ax = ax, with_labels = True, labels = labels, font_size = 8)
+		nx.draw_circular( D, scale = 4, node_size = 150, ax = ax, with_labels = True, labels = labels, font_size = 6 )
 		#nx.draw_networkx_labels(D, pos_labels, ax=ax, labels= labels, font_size = 12)
 		ax.set_title( "alpha = %.2e"%alpha_seq[i])
 
 	plt.savefig( filename )
 
 
-def save_network_graph( matrix, labels, filename, scale=3):
+def save_network_graph( matrix, labels, filename, scale=3, layout = "circular"):
 	labels = dict( zip( range( len(labels) ), labels) )
 	d = matrix.shape[0]
 	D = nx.Graph(matrix)
@@ -85,29 +85,32 @@ def save_network_graph( matrix, labels, filename, scale=3):
 	#weights = weights/np.max( np.abs( weights ) ) 
 	cmap = plt.get_cmap( "jet" ) #or some other one
 	
-	fig = plt.figure()
+	fig = plt.figure(figsize=(7,7))
 	ax = fig.add_subplot(111)
-	pos = nx.circular_layout( D, scale =scale )
-	bweights = [ 5*(1-1*(x<0)) for x in weights ]
-	nx.draw_networkx_edges( D, pos, ax = ax, edge_cmap = cmap, edge_color = bweights, edge_vmin = 0, edge_vmax = 11, width=[10*abs(w)  for w in weights])
+	if layout == "circular":
+		pos = nx.circular_layout( D, scale =scale )
+	elif layout == "spring":
+		pos = nx.spring_layout( D ,scale = scale)
+	#bweights = [ 1+100*(x-min(weights))/( max(weights)- min(weights) ) for x in weights ]
+	bweights = [ 'r'*(z<0) + 'k'*(z>0) for z in weights ]
+	print bweights
+	nx.draw_networkx_edges( D, pos, ax = ax,edge_vmin=0, edge_vmax=100, edge_cmap=cmap, edge_color = bweights, width=[6*np.log( abs(1.3*w)+1)  for w in weights])
 	nx.draw_networkx_nodes( D, pos, ax=ax, node_size = 350, node_color="white")
 	nx.draw_networkx_labels( D, pos,font_size=9, labels = labels, ax = ax)
 	plt.axis("off")
-	plt.savefig( filename)
+	plt.savefig( filename, bbox_inches="tight")
 	return
 	
 	
-	#draw.
 	
 	
 			
 			
  
-	pos_nodes = nx.spring_layout( D, scale = 3 )   
 
 
 
 def absolute_daily_returns( ts) :
-    return (ts['Adj Close']/(ts['Adj Close'].shift(1) )-1)[1:]
+    return (ts['Close']/(ts['Close'].shift(1) )-1)[1:]
 	
 	
